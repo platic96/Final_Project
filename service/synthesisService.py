94ac8547fa92671a230f9185ab3e14e059fa6fa2
Pearty <platic96@gmail.com>
2021-06-28 20:22:09 +0900

    Start

---
+++ service/synthesisService.py
@@ -0,0 +1,5 @@
+from repository.synthesissRepository import synthesissRepository
+
+class SynthesisService:
+    def SynthesisService_record():
+        return synthesissRepository.synthesissRepository_record()
