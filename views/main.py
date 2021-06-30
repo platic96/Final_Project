33ec5598a9e72c7f78add25606a14990a897ab15
HeoYou <lov21y_@naver.com>
2021-06-30 16:04:18 +0900

    main commit

--- views/main.py
+++ views/main.py
@@ -23,6 +23,24 @@ def record():
         print(show_message[i]["message"])
     return render_template("index.html")
 
+@bp.route('/upload', methods=['POST'])
+def upload():
+
+    imd = request.form
+    tmp = imd.getlist('base64')[0]
+    blob = imd.getlist('audio')
+    print(imd)
+    print(blob)
+    
+    tmp = tmp.split(',')
+    # print(tmp)
+    wav_file = open("temp.webm", "wb")   
+    decode_string = base64.b64decode(tmp[1])
+    # print(decode_string)
+    wav_file.write(decode_string)
+        
+    return render_template('question/test.html')
+
 @bp.route('/')
 def index():
     Global_veriable.CTB_v=CTalkBot('02927ec8-57aa-4d20-b468-b06c18dfa8df')
