20cdc53e069dd4f3bc28d14c7f14bf0bcba52517
pearty <platic96@gmail.com>
2021-07-08 00:24:51 +0000

    port and ssl setting

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
-import sys
+import ssl
 from app import app
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+    ssl_context.load_cert_chain(certfile='/usr/test/openssl-1.1.1k/private.crt',keyfile='/usr/test/openssl-1.1.1k/bitasbit.key',password='1234')
+    app.run(host='0.0.0.0',port=5000,ssl_context=ssl_context)
